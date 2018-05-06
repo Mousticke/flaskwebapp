f22bc45927d8eb1e9bec6a51bf4f64a290480be8
Benchiha Akim <akim.benchiha@gmail.com>
2018-05-07 01:07:16 +0200

    Add login and register forms with validation in the front side

--- flaskwebapp.py
+++ flaskwebapp.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'e784ae3c10c94269f19f9bdcaee5f34a'
 
 posts = [
     {
@@ -24,6 +27,25 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About Akim\'World')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register in Akim\'s World', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@akim.com' and form.password.data == 'password':
+            flash('You have been logged in ! ', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check tour credentials', 'danger')
+    return render_template('Login.html', title='Login in Akim\'s World', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)

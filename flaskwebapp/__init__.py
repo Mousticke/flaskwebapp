5ddc1b75f6db107c5a0743c03813c87668e4f3c4
Benchiha Akim <akim.benchiha@gmail.com>
2018-05-07 23:30:40 +0200

    Add custom error pages

--- flaskwebapp/__init__.py
+++ flaskwebapp/__init__.py
@@ -29,8 +29,11 @@ def create_app(config_class=Config):
     from flaskwebapp.users.routes import users
     from flaskwebapp.posts.routes import posts
     from flaskwebapp.main.routes import main
+    from flaskwebapp.errors.handlers import errors
+
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app

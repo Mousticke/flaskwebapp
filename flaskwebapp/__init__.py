e8a03310427e2182bffda5e5973c791ef983f863
Benchiha Akim <akim.benchiha@gmail.com>
2018-05-07 22:09:22 +0200

    Reset password

--- flaskwebapp/__init__.py
+++ flaskwebapp/__init__.py
@@ -1,7 +1,10 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e784ae3c10c94269f19f9bdcaee5f34a'
@@ -11,5 +14,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login' #function name of the route
 login_manager.login_message_category = 'info'
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'FAKE@gmail.com'#os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = 'FAKE' #os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from flaskwebapp import routes
--- flaskwebapp/routes.py
+++ flaskwebapp/routes.py
@@ -2,11 +2,11 @@ import secrets
 import os
 from PIL import  Image
 from flask import render_template, url_for, flash, redirect, request, abort
-from flaskwebapp import app, db, bcrypt
-from flaskwebapp.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from flaskwebapp import app, db, bcrypt, mail
+from flaskwebapp.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, RequestResetForm, ResetPasswordForm
 from flaskwebapp.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
+from flask_mail import Message
 @app.route('/')
 @app.route('/home')
 def home():
@@ -137,3 +137,44 @@ def user_posts(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('user_posts.html', user_posts=posts, user=user)
+
+def send_reset_email(user): 
+    token = user.get_reset_token()
+    message = Message('Password reset request', sender='noreply@demo.com', recipients=[user.email])
+    message.body = f'''
+        To reset you password visit the following link
+        {url_for('reset_token', token=token, _external=True)}
+        If you did not make this request, just simply ignore this email and no changes will be made
+    '''
+    mail.send(message)
+
+@app.route('/reset_password', methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been send with instructions to reset  your password', 'info')
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', title="Reset Password", form=form)
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('This is an invalid or expired toekn', 'warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_pwd = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_pwd
+        db.session.commit()
+        flash('Your password has been updated. You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('reset_token.html', title="Reset Password", form=form)
+    
+

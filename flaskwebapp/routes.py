4c7334a9b246d8a01dfad796c525af2b927acff4
Benchiha Akim <akim.benchiha@gmail.com>
2018-05-07 14:00:19 +0200

    Do some package structure

---
+++ flaskwebapp/routes.py
@@ -0,0 +1,47 @@
+from flask import render_template, url_for, flash, redirect
+from flaskwebapp import app
+from flaskwebapp.forms import RegistrationForm, LoginForm
+from flaskwebapp.models import User, Post
+
+posts = [
+    {
+        'author': 'Corey Schaffer',
+        'title': 'Repository 1',
+        'content': 'Project using python',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Akim Benchiha',
+        'title': 'Repository 2',
+        'content': 'Project using python and flask',
+        'date_posted': 'May 20, 2018'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', home_posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About Akim\'World')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register in Akim\'s World', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@akim.com' and form.password.data == 'password':
+            flash('You have been logged in ! ', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check tour credentials', 'danger')
+    return render_template('Login.html', title='Login in Akim\'s World', form=form)

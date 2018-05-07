ccc7c998409986bee23e15588d158a0026d8bccd
Benchiha Akim <akim.benchiha@gmail.com>
2018-05-07 17:59:19 +0200

    CRUD posts

--- flaskwebapp/forms.py
+++ flaskwebapp/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskwebapp.models import User
 from flask_login import current_user
@@ -63,3 +63,8 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('Please choose a different email.')
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
--- flaskwebapp/routes.py
+++ flaskwebapp/routes.py
@@ -1,30 +1,16 @@
 import secrets
 import os
 from PIL import  Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskwebapp import app, db, bcrypt
-from flaskwebapp.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskwebapp.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskwebapp.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
-posts = [
-    {
-        'author': 'Corey Schaffer',
-        'title': 'Repository 1',
-        'content': 'Project using python',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Akim Benchiha',
-        'title': 'Repository 2',
-        'content': 'Project using python and flask',
-        'date_posted': 'May 20, 2018'
-    }
-]
 
 @app.route('/')
 @app.route('/home')
 def home():
+    posts = Post.query.all()
     return render_template('home.html', home_posts=posts)
 
 @app.route('/about')
@@ -97,3 +83,49 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account in Akim\'s World', image_file=image_file, form=form)
+
+@app.route("/posts/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title="New Post", form=form, legend='New Post')
+
+@app.route("/posts/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@app.route("/posts/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated. ', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content  
+    return render_template('create_post.html', title="Update Post", form=form, legend='Update Post')
+
+@app.route("/posts/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted. ', 'success')
+    return redirect(url_for('home'))

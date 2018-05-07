eecdf0a1c29a4803133db31ee934c7e287d950a5
Benchiha Akim <akim.benchiha@gmail.com>
2018-05-07 16:45:24 +0200

    User account management

--- flaskwebapp/forms.py
+++ flaskwebapp/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskwebapp.models import User
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -38,3 +40,26 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')   
      
     submit = SubmitField('Sign in')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+     validators=[DataRequired(), Length(min=2, max=20)])
+    
+    email = StringField('Email',
+     validators=[DataRequired(), Email()])
+
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png', 'gif'])])
+    
+    submit = SubmitField('Update account')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Please choose a different username.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Please choose a different email.')

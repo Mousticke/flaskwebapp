39d1cd14b156a6555b780bd335b655eb0d57f5a0
Benchiha Akim <akim.benchiha@gmail.com>
2018-05-07 15:58:28 +0200

    User authentication

--- flaskwebapp/__init__.py
+++ flaskwebapp/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e784ae3c10c94269f19f9bdcaee5f34a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' #function name of the route
+login_manager.login_message_category = 'info'
 
 from flaskwebapp import routes
--- flaskwebapp/forms.py
+++ flaskwebapp/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskwebapp.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -16,6 +17,16 @@ class RegistrationForm(FlaskForm):
      validators=[DataRequired(), EqualTo('password')])
     
     submit = SubmitField('Sign up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Please choose a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Please choose a different email.')
 
 class LoginForm(FlaskForm):    
     email = StringField('Email',
